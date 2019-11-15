87329e28c7d6f88cbe586ca9aab724f4265f1c20
aoleary <seanm187@gmail.com>
2019-11-15 14:38:36 +0000

    Xtend the h811

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,7 +16,7 @@
 # the License for the specific language governing 
 # permissions and limitations under the License.
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_h811.mk
+    $(LOCAL_DIR)/xtended_h811.mk
 
 # Screen density
 # must be defined here as they get readonly later on
